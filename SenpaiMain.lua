2f3aa3b7ab6c356e9be834237405c1da647ce2e7
Vzltex <62889270+Vzltex@users.noreply.github.com>
2020-05-18 10:32:45 +0700

    Create SenpaiMain.lua

---
+++ SenpaiMain.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 3851622790 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Vzltex/Senpai-Lib/master/Break%20In.lua'),true))()
+else
+    print"nogamedetect"
+end
